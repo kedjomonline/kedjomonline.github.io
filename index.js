9e00ddf586d217f411a66123028f40ab17f69b4f
Bonaventure Fonteh <kiehfonteh@gmail.com>
2023-10-05 05:57:34 +0100

    Add all files to website

---
+++ index.js
@@ -0,0 +1,5 @@
+var date = new Date();
+var dateHolder = document.getElementById("date")
+dateHolder.innerText = "©" +date.getFullYear() + " Kəjôm Keku Online";
+dateHolder.style.color = "whitesmoke";
+ 
